f0cd3666f8670f4ac007ce6f5e6fb1efc4f6575f
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-20 14:55:12 -0300

    Instanciado objeto Carro, mostrado os atributos do objeto, aplicado dois métodos e mostrado o resultado.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner teclado = new Scanner(System.in);
+
+        Carro chevrolet = new Carro("vectra", 1400,"chevrolet", "azul", 25000);
+
+        System.out.println("Nome: " + chevrolet.nome + " Peso: " + chevrolet.peso + " Fabricante: " + chevrolet.fabricante + " Cor: " + chevrolet.cor + " Valor do Carro: " + chevrolet.valorDoCarro);
+        chevrolet.darDesconto(0.1);
+        chevrolet.pintar("preto");
+
+        System.out.println("Nome: " + chevrolet.nome + " Peso: " + chevrolet.peso + " Fabricante: " + chevrolet.fabricante + " Cor: " + chevrolet.cor + " Valor do Carro: " + chevrolet.valorDoCarro);
+
+
+
+
+
+        teclado.close();
     }
 }
