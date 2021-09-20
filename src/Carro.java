57889af37aa5ecc6323a04c158360d358af0d1f0
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-20 14:40:38 -0300

    Declaração dos atributos, método construtor e métodos da classe Carro. declaração do método de entrada na classe Main.

--- src/Carro.java
+++ src/Carro.java
@@ -1,2 +1,26 @@
 public class Carro {
+
+    //declarando atributos
+    String nome;
+    double peso;
+    String fabricante;
+    String cor;
+    double valorDoCarro;
+
+    //método construtor
+    public Carro(String nome, double peso, String fabricante, String cor, double valorDoCarro) {
+        this.nome = nome;
+        this.peso = peso;
+        this.fabricante = fabricante;
+        this.cor = cor;
+        this.valorDoCarro = valorDoCarro;
+    }
+
+    public void darDesconto(double percentualDeDesconto){
+        this.valorDoCarro = valorDoCarro - (valorDoCarro * percentualDeDesconto);
+    }
+
+    public void pintar(String novaCor){
+        this.cor = novaCor;
+    }
 }
